435bce75a806b921749c434ecde0da063a81d9ca
David Borja <DBorja95@administrators-iMac.local>
2022-07-06 08:43:20 -0600

    done

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
 // VoiceRSS Javascript SDK
+
 const VoiceRSS = {
   speech: function (e) {
     this._validate(e), this._request(e);
@@ -102,10 +106,16 @@ const VoiceRSS = {
   },
 };
 
-function test() {
+// Disable/Enable button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// passing joke to voiceRSS API
+function tellMe(joke) {
   VoiceRSS.speech({
     key: "<8140ae0c013646a8871ec20cbba10855>",
-    src: "Hello, world!",
+    src: joke,
     hl: "en-us",
     r: 0,
     c: "mp3",
@@ -113,4 +123,26 @@ function test() {
     ssml: false,
   });
 }
-test();
+
+// get jokes form joke api
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,sexist,explicit";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // TTS
+    tellMe(joke);
+    // disable button
+    toggleButton();
+  } catch (error) {
+    console.error("whoops", error);
+  }
+}
